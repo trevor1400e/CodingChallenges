093d9024f0b87951bd0836e8ee59d0ff14dc2c2d
trevor1400e <thuff2015@gmail.com>
2023-01-02 13:48:04 -0600

    fixed spacing oops.

--- Python/likes.py
+++ Python/likes.py
@@ -18,7 +18,7 @@
 def return_likes(names):
     match len(names):
         case 0:
-           return 'no one likes this'
+            return 'no one likes this'
         case 1:
             return names[0]+' likes this'
         case 2:
